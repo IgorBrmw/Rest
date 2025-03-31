c8798f65520fc371dfbbf01e018b33eb53c60ddd
Igor Burmistrov <igor.brmw@gmail.com>
2025-03-31 20:35:36 +0300

    third commit

--- src/main/java/com/example/IgorWebApp30/Application.java
+++ src/main/java/com/example/IgorWebApp30/Application.java
@@ -3,6 +3,10 @@ package com.example.IgorWebApp30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +14,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
--- src/main/java/com/example/IgorWebApp30/repository/RoleRepository.java
+++ src/main/java/com/example/IgorWebApp30/repository/RoleRepository.java
@@ -2,10 +2,6 @@ package com.example.IgorWebApp30.repository;
 
 import com.example.IgorWebApp30.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
